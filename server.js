d2f78b13018ed5c244179430b86f966f119ab1de
muchoki769 <ndungudavidmuchoki@gmail.com>
2025-10-04 14:42:50 +0300

    changes in server,js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
   {
-  origin: 'http://localhost:5173', 
+  origin: process.env.Frontend || 'http://localhost:5173', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
